b38e5d20ddbc5b721205268af835868ee3c7c5f3
Peter Schorn <petervschorn@gmail.com>
2022-12-03 05:44:32 -0600

    Added aws folder with Dockerrun.aws.json.

---
+++ push_ecr.sh
@@ -0,0 +1,5 @@
+aws ecr get-login-password --region us-east-1 | \
+docker login --username AWS --password-stdin 947734355387.dkr.ecr.us-east-1.amazonaws.com
+
+docker build . -t 947734355387.dkr.ecr.us-east-1.amazonaws.com/todo:latest
+docker push 947734355387.dkr.ecr.us-east-1.amazonaws.com/todo:latest
